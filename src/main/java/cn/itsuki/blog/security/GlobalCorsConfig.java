cdcf146c2adc9020d57165f4284e8c61cc66f6ed
itsuki0927 <2309899048@qq.com>
2021-12-21 09:17:14 +0800

    chore: remove test environment cors config

--- src/main/java/cn/itsuki/blog/security/GlobalCorsConfig.java
+++ src/main/java/cn/itsuki/blog/security/GlobalCorsConfig.java
@@ -19,8 +19,6 @@ public class GlobalCorsConfig {
     private String webUrl;
     @Value("${cors.adminUrl}")
     private String adminUrl;
-    @Value("${cors.testUrl}")
-    private String testUrl;
     @Value("${cors.vercelUrl}")
     private String vercelUrl;
 
@@ -33,7 +31,6 @@ public class GlobalCorsConfig {
         //允许指定域名进行跨域调用
         config.addAllowedOrigin(webUrl);
         config.addAllowedOrigin(adminUrl);
-        config.addAllowedOrigin(testUrl);
         config.addAllowedOrigin(vercelUrl);
         //允许跨越发送cookie
         config.setAllowCredentials(true);
