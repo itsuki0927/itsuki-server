a963f34ff35a8fc467c34916018a03243ded4063
itsuki0927 <2309899048@qq.com>
2022-05-10 16:30:27 +0800

    chore: clean article id rest api

--- src/main/java/cn/itsuki/blog/controllers/ArticleController.java
+++ src/main/java/cn/itsuki/blog/controllers/ArticleController.java
@@ -1,15 +1,9 @@
 package cn.itsuki.blog.controllers;
 
-import cn.itsuki.blog.entities.Article;
 import cn.itsuki.blog.entities.ArticleArchive;
 import cn.itsuki.blog.entities.ArticleId;
-import cn.itsuki.blog.entities.requests.ArticleCreateRequest;
-import cn.itsuki.blog.entities.requests.ArticleMetaPatchRequest;
-import cn.itsuki.blog.entities.requests.ArticlePatchRequest;
 import cn.itsuki.blog.entities.requests.ArticleSearchRequest;
-import cn.itsuki.blog.entities.responses.ArticleDetailResponse;
 import cn.itsuki.blog.entities.responses.ArticleSummaryResponse;
-import cn.itsuki.blog.entities.responses.SearchResponse;
 import cn.itsuki.blog.entities.responses.WrapperResponse;
 import cn.itsuki.blog.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,11 +39,6 @@ public class ArticleController {
     @GetMapping("/archive")
     public WrapperResponse<TreeMap<String, TreeMap<String, List<ArticleArchive>>>> archive() {
         return WrapperResponse.build(service.getArchive());
-    }
-
-    @GetMapping("/{id}")
-    public WrapperResponse<ArticleDetailResponse> get(@PathVariable("id") Long id) {
-        return WrapperResponse.build(service.detail(id));
     }
 
     @GetMapping("/count")
