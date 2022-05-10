8335feeea8c7e398cd780ec0b92de29f4238bccb
itsuki0927 <2309899048@qq.com>
2022-05-10 16:36:58 +0800

    feat: add graphql execution log

---
+++ src/main/java/cn/itsuki/blog/instrumentation/RequestLoggingInstrumentation.java
@@ -0,0 +1,39 @@
+package cn.itsuki.blog.instrumentation;
+
+import graphql.ExecutionResult;
+import graphql.execution.instrumentation.InstrumentationContext;
+import graphql.execution.instrumentation.SimpleInstrumentation;
+import graphql.execution.instrumentation.SimpleInstrumentationContext;
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+
+/**
+ * @author: itsuki
+ * @create: 2022-05-10 14:40
+ **/
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RequestLoggingInstrumentation extends SimpleInstrumentation {
+    private final Clock clock = Clock.systemDefaultZone();
+
+    @Override
+    public InstrumentationContext<ExecutionResult> beginExecution(InstrumentationExecutionParameters parameters) {
+        var start = Instant.now(clock);
+        log.info("query: {} with variables: {}", parameters.getQuery(), parameters.getVariables());
+        return SimpleInstrumentationContext.whenCompleted(((executionResult, throwable) -> {
+            var duration = Duration.between(start, Instant.now(clock));
+            if (throwable == null) {
+                log.info("{}: completed successfully in: {}", parameters.getQuery(), duration);
+            } else {
+                log.warn("{}: failed in : {}", parameters.getQuery(), duration, throwable);
+            }
+        }));
+    }
+}
