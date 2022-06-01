1cd6aa4f9ed8d22a68aef6d63cde47e7af753b7a
itsuki0927 <2309899048@qq.com>
2022-06-01 17:07:51 +0800

    feat: add upload image tinify optimize handler

--- src/main/java/cn/itsuki/blog/services/UploadService.java
+++ src/main/java/cn/itsuki/blog/services/UploadService.java
@@ -4,11 +4,15 @@ import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
+import com.tinify.Source;
+import com.tinify.Tinify;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * 上传 服务
@@ -34,21 +38,24 @@ public class UploadService {
     @Value("${qiniu.bucket}")
     private String bucket;
 
+    @Value("${tinify.secretKey}")
+    private String tinifySecretKey;
+
     //构造一个带指定Region对象的配置类
     private Configuration cfg = new Configuration(Region.region0());
 
     private UploadManager uploadManager = new UploadManager(cfg);
 
     public String uploadFile(String prefix, MultipartFile file) {
+        Tinify.setKey(tinifySecretKey);
         Auth auth = Auth.create(accessKey, secretKey);
         String token = auth.uploadToken(bucket);
         try {
             String originalFilename = file.getOriginalFilename();
-            String path = originalFilename;
-            if (prefix != null) {
-                path = prefix + "/" + originalFilename;
-            }
-            uploadManager.put(file.getInputStream(), path, token, null, null);
+            Source source = Tinify.fromBuffer(file.getBytes());
+            String path = prefix != null ? prefix + "/" + originalFilename : originalFilename;
+            InputStream inputStream = new ByteArrayInputStream(source.result().toBuffer());
+            uploadManager.put(inputStream, path, token, null, null);
             return path;
         } catch (IOException e) {
             e.printStackTrace();
