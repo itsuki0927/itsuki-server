4bb463b00765d4b2d0d8185306372de90b1b1c79
itsuki0927 <2309899048@qq.com>
2022-06-01 09:33:34 +0800

    fix: use json string replace read resource google_secret.json

--- src/main/java/cn/itsuki/blog/services/SeoService.java
+++ src/main/java/cn/itsuki/blog/services/SeoService.java
@@ -12,6 +12,7 @@ import com.google.api.client.json.gson.GsonFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
@@ -30,6 +31,27 @@ public class SeoService {
     private String token;
     @Value("${mode}")
     private String mode;
+    @Value("${seo.google.type")
+    private String type;
+    @Value("${seo.google.projectId")
+    private String projectId;
+    @Value("${seo.google.privateKeyId")
+    private String privateKeyId;
+    @Value("${seo.google.privateKey")
+    private String privateKey;
+    @Value("${seo.google.clientEmail")
+    private String clientEmail;
+    @Value("${seo.google.clientId")
+    private String clientId;
+    @Value("${seo.google.authUri")
+    private String authUri;
+    @Value("${seo.google.tokenUri")
+    private String tokenUri;
+    @Value("${seo.google.authProviderCertUrl")
+    private String authProviderCertUrl;
+    @Value("${seo.google.clientCertUrl")
+    private String clientCertUrl;
+
 
     public boolean isProd() {
         return mode.equals("prod");
@@ -70,6 +92,7 @@ public class SeoService {
         String type = getGoogleActionString(action);
         String actionText = "Google ping [" + type + "] action";
         String url = urls.get(0);
+        String secretJson = buildGoogleSecretJson();
 
         System.out.println(actionText);
 
@@ -77,7 +100,8 @@ public class SeoService {
             JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
             HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
-            InputStream secretInput = getClass().getResourceAsStream("/google_secret.json");
+            InputStream secretInput = new ByteArrayInputStream(secretJson.getBytes());
+            System.out.println(secretInput.toString());
             GoogleCredential credentials = GoogleCredential.fromStream(secretInput, httpTransport, jsonFactory).createScoped(Collections.singleton(scopes));
 
             GenericUrl genericUrl = new GenericUrl(endPoint);
@@ -93,9 +117,25 @@ public class SeoService {
             HttpResponse response = request.execute();
 
             System.out.println(IoUtil.read(response.getContent()).toString());
+            System.out.println("ping google success");
         } catch (GeneralSecurityException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String buildGoogleSecretJson() {
+        return "{"
+                + "\"type\": \"" + type + "\","
+                + "\"project_id\": \"" + projectId + "\","
+                + "\"private_key_id\": \"" + privateKeyId + "\","
+                + "\"private_key\": \"" + privateKey + "\","
+                + "\"client_email\": \"" + clientEmail + "\","
+                + "\"client_id\": \"" + clientId + "\","
+                + "\"auth_uri\": \"" + authUri + "\","
+                + "\"token_uri\": \"" + tokenUri + "\","
+                + "\"auth_provider_x509_cert_url\": \"" + authProviderCertUrl + "\","
+                + "\"client_x509_cert_url\": \"" + clientCertUrl + "\""
+                + "}";
     }
 
     // Baidu
